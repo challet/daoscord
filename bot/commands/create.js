b7ac21dacda4c9439ef6e305e4c682d56d4aac61
Clément Hallet <clement@challet.eu>
2023-07-23 08:09:17 +0200

    user management commands (#13)

--- bot/commands/create.js
+++ bot/commands/create.js
@@ -1,3 +1,19 @@
-export default function(interaction) {
-  interaction.reply({ content: 'Create DAO command aknowledged' })
+import readUSerFactory from '../../lib/src/readUser.js'
+import dfnsConfig from '../dfnsConfig.js'
+
+const readUser = readUSerFactory(dfnsConfig)
+
+export default async (interaction, daoClient) => {
+  await interaction.deferReply()
+
+  await 
+  
+  daoClient.createDao()
+    .then((address) => {
+      interaction.editReply({ content: `DAO deployed on https://mumbai.polygonscan.com/address/${address}` })
+    })
+    .catch((error) => {
+      interaction.editReply({ content: 'Error: ```' + error + '```' })
+    })
+
 }
--- bot/commands/whoami.js
+++ bot/commands/whoami.js
@@ -1,3 +1,17 @@
-export default function(interaction) {
-  interaction.reply({ content: 'Who ami I in the DAO command aknowledged' })
+import readUSerFactory from '../../lib/src/readUser.js'
+import dfnsConfig from '../dfnsConfig.js'
+
+const readUser = readUSerFactory(dfnsConfig)
+
+export default async (interaction) => {
+  await interaction.deferReply()
+  
+  readUser(interaction.user.id)
+    .then((data) => {
+      interaction.editReply({ content: `Status ${data.status} since ${data.createdAt}.\n Your wallet is https://mumbai.polygonscan.com/address/${data.address}. You have ${data.balance} voting power` })
+    })
+    .catch((error) => {
+      interaction.editReply({ content: 'Error: ```' + error + '```' })
+    })
+
 }
